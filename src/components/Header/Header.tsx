f1ba0b226844da0612df45c8bc9cae34736f867c
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-02-25 01:01:37 +0100

    Header is 50% complete

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,28 @@
+import { FaBars, FaTimes } from "react-icons/fa";
 export const Header = () => {
-    return <header className={`bg-black max-w-[100vw] p-[1.5rem] h-[5rem] `}>
-      
-  </header>;
+  return (
+    <header
+      className={` bg-black text-cyan-600 max-w-[100vw] py-[1.5rem] px-2 h-[5rem] flex justify-between `}>
+      {/* logo */}
+      <a href='kbjbs' title='go to home page' className='logo'>
+        <img
+          src='/Assets/images/logo.png'
+          alt='logo'
+          className='w-[7rem] object-cover'
+        />
+      </a>
+
+      {/* navbar activator */}
+      <div className=' md:hidden border bordeer-cyan-700 w-fit p-1 '>
+        <FaBars
+          size={30}
+          className='text-white hover:text-cyan-600 hover:cursor-pointer'
+        />
+        <FaTimes
+          className='hidden hover:text-cyan-600 hover:cursor-pointer'
+          size={30}
+        />
+      </div>
+    </header>
+  );
 };
