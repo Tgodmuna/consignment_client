719d6941ddcc4c70c3c572e34e5921403949e07f
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-02-27 22:14:37 +0100

    Fix

--- src/components/Section_3/CallToAction.tsx
+++ src/components/Section_3/CallToAction.tsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 
 export const CallToAction = () => {
+  const navigate = useNavigate();
   return (
     <div className='bg-black p-2 flex flex-col gap-y-5 items-center justify-center py-3'>
       <h1 className='text-white capitalize text-xs font-bold text-center '>
@@ -7,14 +9,20 @@ export const CallToAction = () => {
       </h1>
       <p className='text-white text-center capitalize font-extralight'>
         sign-up now to start shipping with{" "}
-        <span className='text-3xl text-lime-100 uppercase flashing-text'>TrustGold</span>
+        <span className='text-3xl text-lime-100 uppercase flashing-text'>
+          TrustGold
+        </span>
       </p>
 
       <div className='flex  gap-2  '>
-        <button className='rounded-lg text-center p-2 capitalize text-black bg-white hover:text-white hover:bg-black '>
+        <button
+          onClick={() => navigate("jkjbjb")}
+          className='rounded-lg text-center p-2 capitalize text-black bg-white hover:text-white hover:bg-black '>
           personal courier
         </button>
-        <button className='rounded-lg text-center p-2 capitalize text-black bg-white hover:text-white hover:bg-black '>
+        <button
+          onClick={() => navigate("jkjbjb")}
+          className='rounded-lg text-center p-2 capitalize text-black bg-white hover:text-white hover:bg-black '>
           Business shipment
         </button>
       </div>
