4a449a596f4f5831b4d9118eba2d61b965425dc2
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-02-28 22:15:03 +0100

    modification: integrated registration component in CalltoAction

--- src/components/Section_3/CallToAction.tsx
+++ src/components/Section_3/CallToAction.tsx
@@ -1,7 +1,6 @@
-import { useNavigate } from "react-router-dom";
+import ShippingForm from "../Auth/Registration";
 
 export const CallToAction = () => {
-  const navigate = useNavigate();
   return (
     <div className='bg-black p-2 flex flex-col gap-y-5 items-center justify-center py-3'>
       <h1 className='text-white capitalize text-xs font-bold text-center '>
@@ -13,19 +12,7 @@ export const CallToAction = () => {
           TrustGold
         </span>
       </p>
-
-      <div className='flex  gap-2  '>
-        <button
-          onClick={() => navigate("jkjbjb")}
-          className='rounded-lg text-center p-2 capitalize text-black bg-white hover:text-white hover:bg-black '>
-          personal courier
-        </button>
-        <button
-          onClick={() => navigate("jkjbjb")}
-          className='rounded-lg text-center p-2 capitalize text-black bg-white hover:text-white hover:bg-black '>
-          Business shipment
-        </button>
-      </div>
+      <ShippingForm />
     </div>
   );
 };
