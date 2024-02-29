bfb90530f01738f551944b0990ef83814cb1ba35
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-02-29 06:36:04 +0100

    New Feature:

--- src/components/Section_3/CallToAction.tsx
+++ src/components/Section_3/CallToAction.tsx
@@ -5,10 +5,10 @@ export const CallToAction = () => {
     <div
       id='section6'
       className='bg-black p-2 flex flex-col gap-y-5 items-center justify-center py-3'>
-      <h1 className='text-white capitalize text-xs font-bold text-center '>
+      <h1 className='text-white capitalize text-xs font-bold text-center md:text-xl md:w-full'>
         experience safe , easy and relieable shipping in UK and around the world
       </h1>
-      <p className='text-white text-center capitalize font-extralight'>
+      <p className='text-white text-center capitalize font-extralight md:text-pretty'>
         sign-up now to start shipping with{" "}
         <span className='text-3xl text-lime-100 uppercase flashing-text'>
           TrustGold
