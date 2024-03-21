31fff862283d322da7b5cf15dab017a6eeffda91
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-03-21 07:08:29 +0100

    Feature: integerated maker icon on the map to show the exact Parcel location.       integerated  Leaflet map on each  user parcel passing coordinates as a props for tracking the parcel location

--- src/components/Dashboard/OrderList.tsx
+++ src/components/Dashboard/OrderList.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import LoadingSpinner from "../utilities/Spinner";
+import MapWithMarker from "../utilities/MapWithMarker";
 
 interface Order {
   sender: string;
@@ -17,6 +18,7 @@ interface Order {
 const OrderList: React.FC = () => {
   const [OrderList, setOrderList] = useState<Order[]>([]);
   const [IsLoading, setIsLoading] = useState(false);
+  const [ErrorMesage, setErrorMesage] = useState("");
 
   // on component mount fetch orders list
   useEffect(() => {
@@ -39,6 +41,7 @@ const OrderList: React.FC = () => {
         .catch((err) => {
           console.log(err);
           setIsLoading(false);
+          setErrorMesage(err.message);
         });
     } else {
       setIsLoading(false);
@@ -47,24 +50,28 @@ const OrderList: React.FC = () => {
   }, []);
 
   return (
-    <div className='m-auto overflow-auto h-full overflow-x-hidden py-[2rem] flex flex-col items-center justify-center bg-slate-50 w-full'>
+    <div className=' overflow-scroll h-[40rem] flex-wrap m-auto items-center justify-center bg-slate-50 w-full'>
       {IsLoading && (
-        <LoadingSpinner className='h-[5rem] w-[10rem] bg-green-400' />
+        <LoadingSpinner className='h-[5rem] w-[10rem]  m-auto' />
       )}
-      <h2 className='text-2xl capitalize font-bold mb-4'>
+      <h2 className='text-2xl capitalize font-bold mb-4 text-center'>
         list of orders made ({OrderList.length})
       </h2>
-      <div className='flex flex-wrap bg-red-900 items-center justify-center h-fill overflow-scroll'>
-        {OrderList.map((order, index) => (
-          <div
-            key={index}
-            className='flex flex-col gap-3 p-2 w-[25vw] m-2 bg-slate-200 shadow-lg rounded-md'>
-            <p className='font-bold'>Sender: {order.sender}</p>
-            <p className='font-bold'>Recipient: {order.recipient}</p>
-            <p className='font-bold'>Destination: {order.parcelLocation}</p>
-          </div>
-        ))}
-      </div>
+      {ErrorMesage ? (
+        <h1 className='text-red-500 text-2xl text-center '>{ErrorMesage}</h1>
+      ) : (
+        ""
+      )}
+      {OrderList.map((order, index) => (
+        <div
+          key={index}
+          className='flex flex-col m-auto gap-3 p-2 w-[70vw] my-2 bg-slate-200 shadow-lg rounded-md'>
+          <p className='font-bold'>Sender: {order.sender}</p>
+          <p className='font-bold'>Recipient: {order.recipient}</p>
+          <p className='font-bold'>Destination: {order.parcelLocation}</p>
+          <MapWithMarker coordinates={order.coordinates} />
+        </div>
+      ))}
     </div>
   );
 };
