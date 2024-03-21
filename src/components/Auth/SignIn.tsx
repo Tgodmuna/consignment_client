e5530c38cc41163aed11e76547ec3481a0e83821
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-03-21 21:59:31 +0100

    Bug: adjust form component to center both in X and Y axis

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -39,9 +39,12 @@ const SignInForm = () => {
         setMessage(res.data.message);
 
         if (message === "Login successful" && status === 200) {
-          sessionStorage.setItem("login token", token);
+          sessionStorage.setItem("login-token", token);
           localStorage.setItem("userID", res.data.user.ID);
           navigate("/dashboard");
+        } else if (status === 200 && res.data.userName === "admin") {
+          sessionStorage.setItem("adminToken", res.data.token);
+          navigate("/Admin");
         }
       })
       .catch((er) => {
@@ -53,58 +56,56 @@ const SignInForm = () => {
   };
 
   return (
-    <div className='mx-auto pt-16 rounded-xl shadow-md border md:max-w-full flex flex-col items-center justify-centery'>
-      <form
-        className='md:w-1/2 w-full p-8 rounded-lg flex flex-col items-center justify-center bg-gray-100 border-2 border-cyan-500'
-        onSubmit={handleSubmit}>
-        <div className='mb-4 w-full'>
-          <input
-            type='email'
-            name='email'
-            value={formData.email}
-            onChange={handleChange}
-            className='w-full border-gray-300 rounded-md px-4 py-2 border'
-            placeholder='Email'
-            required
-          />
-        </div>
+    <form
+      className=' form- max-w-[50rem] max-h-[100%] m-auto w-full mt-[5rem] p-8 rounded-lg flex flex-col items-center justify-center gap-[3rem] bg-gray-100 border-2 border-cyan-500'
+      onSubmit={handleSubmit}>
+      <div className='mb-4 w-full'>
+        <input
+          type='email'
+          name='email'
+          value={formData.email}
+          onChange={handleChange}
+          className='w-full border-gray-300 rounded-md px-4 py-2 border'
+          placeholder='Email'
+          required
+        />
+      </div>
 
-        <div className='mb-4 w-full'>
-          <input
-            type='phone'
-            name='phone'
-            value={formData.phone}
-            onChange={handleChange}
-            className='w-full border-gray-300 rounded-md px-4 py-2 border'
-            placeholder='phone'
-            required
-          />
-        </div>
+      <div className='mb-4 w-full'>
+        <input
+          type='phone'
+          name='phone'
+          value={formData.phone}
+          onChange={handleChange}
+          className='w-full border-gray-300 rounded-md px-4 py-2 border'
+          placeholder='phone'
+          required
+        />
+      </div>
 
-        <p
-          className={`${
-            Message === "No record" ? "block" : "hidden"
-          } text-red-500`}>
-          no user with {Message} found,try signin up
-        </p>
-        <p className={`${NetworkError ? "block text-red-500" : "hidden"}`}>
-          {NetworkError}
-        </p>
+      <p
+        className={`${
+          Message === "No record" ? "block" : "hidden"
+        } text-red-500`}>
+        no user with {Message} found,try signin up
+      </p>
+      <p className={`${NetworkError ? "block text-red-500" : "hidden"}`}>
+        {NetworkError}
+      </p>
 
-        {/* button */}
-        <div className='my-[1rem] pb-5'>
-          <button
-            ref={BTNref}
-            type='submit'
-            className={`bg-blue-500 cursor-pointer ${
-              IsLoading ? "cursor-not-allowed opacity-70" : "cursor-default"
-            } text-white px-4 py-2 rounded-md hover:bg-blue-600 border  items-center flex w-fit gap-2`}>
-            Submit
-            {IsLoading && <LoadingSpinner className='bg-green-500' />}
-          </button>
-        </div>
-      </form>
-    </div>
+      {/* button */}
+      <div className='my-[1rem] pb-5'>
+        <button
+          ref={BTNref}
+          type='submit'
+          className={`bg-blue-500 cursor-pointer ${
+            IsLoading ? "cursor-not-allowed opacity-70" : "cursor-default"
+          } text-white px-4 py-2 rounded-md hover:bg-blue-600 border  items-center flex w-fit gap-2`}>
+          Submit
+          {IsLoading && <LoadingSpinner className='bg-green-500' />}
+        </button>
+      </div>
+    </form>
   );
 };
 
