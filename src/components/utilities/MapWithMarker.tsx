dc696c9d8d438993cfb9b30bbce74c6eb07d1355
ThankGod Munachimso Agu <AguthankGod@gmail.com>
2024-03-24 07:43:12 +0100

    Revert "Feature: on marker pop, set the location on the pop"
    This reverts commit d8192b7ba3ccc55ff96d911404993439fe9e25bf.

--- src/components/utilities/MapWithMarker.tsx
+++ src/components/utilities/MapWithMarker.tsx
@@ -1,8 +1,7 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Icon from "./Icon";
-import axios from "axios";
 
 interface MapWithMarkerProps {
   coordinates: { lat: number; lon: number } | undefined;
@@ -10,34 +9,6 @@ interface MapWithMarkerProps {
 
 const MapWithMarker: React.FC<MapWithMarkerProps> = ({ coordinates }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [LocationName, setLocationName] = useState<string | undefined>("");
-  const [isLocationNameLoading, setisLocationNameLoading] = useState(false);
-  const [locationNameError, setlocationNameError] = useState("");
-  const [isErr, setisErr] = useState(false);
-
-  //get the location name using the  coordinates passed as prop
-  const GetLocationName = useCallback(
-    (coordinates: { lat: number; lon: number }) => {
-      setisLocationNameLoading(true);
-      setisErr(false);
-      axios
-        .get(
-          `https://nominatim.openstreetmap.org/reverse?lat=${coordinates.lat}&lon=${coordinates.lon}&format=json`,
-        )
-        .then((res) => {
-          setLocationName(res.data.display_name);
-          setisLocationNameLoading(false);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err.message);
-          setisLocationNameLoading(false);
-          setlocationNameError(err.message);
-          setisErr(true);
-        });
-    },
-    [],
-  );
 
   return (
     <div style={{ width: "100%", height: "400px" }}>
@@ -53,33 +24,17 @@ const MapWithMarker: React.FC<MapWithMarkerProps> = ({ coordinates }) => {
             eventHandlers={{
               mouseover: () => setIsPopupOpen(true),
               mouseout: () => setIsPopupOpen(false),
-              click: () => {
-                GetLocationName(coordinates);
-              },
             }}
             title='hover or click to see parcel location'
             position={[coordinates.lat, coordinates.lon]}
             icon={Icon}
             alt='current parcel location'>
             {isPopupOpen && (
-              <Popup className='capitalize text-neutral-600' autoClose={true}>
-                {isLocationNameLoading ? (
-                  <span className='animate-pulse text-yellow-900 text-xs capitalize'>
-                    getting location name
-                  </span>
-                ) : isErr ? (
-                  <span className='text-red-700 uppercase text-xs'>
-                    {locationNameError}
-                  </span>
-                ) : (
-                  <span className='text-green-600 flex flex-col'>
-                    <span className='font-bold text-center'>
-                      current parcel location:
-                    </span>{" "}
-                    <br /> {LocationName}
-                  </span>
-                )}
-              </Popup>
+              <Popup
+                className='capitalize text-neutral-600'
+                autoClose={true}
+                content={"current location of the parcel"}
+              />
             )}
           </Marker>
         )}
